f3fd087d0d5e53e3f0d216b2517b886722d50041
Gaven Rendell <gaven@rendello.ca>
2021-01-22 16:03:51 -0500

    Add node sorting

--- main.zig
+++ main.zig
@@ -1,17 +1,16 @@
 const std = @import("std");
 const print = std.debug.print;
-const ArrayList = std.ArrayList;
 var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 
 const NodeTag = enum {
     internal,
     leaf
 };
-        //left: Node, // needs pointer
-        //right: Node,
 
 const Node = union(NodeTag) {
     internal: struct {
+        //left: Node, // needs pointer
+        //right: Node,
         count: u64
     },
     leaf: struct {
@@ -20,20 +19,36 @@ const Node = union(NodeTag) {
     }
 };
 
-fn build_table(text: []const u8) !void {
-    var node_list = ArrayList(Node).init(&gpa.allocator);
+/// Compare nodes by count. Descending.
+fn compare_nodes(ctx: void, a: Node, b: Node) bool {
+    return a.leaf.count < b.leaf.count;
+}
 
-    for (text) |byte, i| {
-        for (node_list.items) |*node| {
+fn build_table(text: []const u8) !void {
+    var nodes = std.ArrayList(Node).init(&gpa.allocator);
+
+    outer: for (text) |byte, i| {
+        var found: bool = false;
+
+        for (nodes.items) |*node| {
             if (byte == node.leaf.byte) {
                 node.leaf.count += 1;
+                continue :outer;
             }
         }
+        if (!found) {
+            var node = Node { .leaf = .{ .byte = byte, .count = 1 } };
+            try nodes.append(node);
+        }
+    }
+    std.sort.sort(Node, nodes.items, {}, compare_nodes);
+    for (nodes.items) |node| {
+        print("{c} {}\n", .{node.leaf.byte, node.leaf.count});
     }
 }
 
 pub fn main() !void {
     print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n============\n", .{});
-    try build_table("Hello, world! It's-a-me, Marioooo!");
+    try build_table("Hello, world! It's-a-me, Marioooo! I listen to ZZ Top!");
 }
 
