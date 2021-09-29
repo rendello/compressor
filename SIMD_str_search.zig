3ba41bb5dca68b7b3660ded43dd7658f1707d79a
Gaven Rendell <gaven@rendello.ca>
2021-09-29 12:22:16 -0400

    More work on SIMD search

--- SIMD_str_search.zig
+++ SIMD_str_search.zig
@@ -1,6 +1,6 @@
 // Gaven Rendell, 2021.
 
-//! Reference: "SIMD-friendly algorithms for substring searching" by Wojciech Muła
+//! Reference: "SIMD-friendly algorithms for substring searching" by Wojciech Muła.
 
 const std = @import("std");
 const ArrayList = std.ArrayList;
@@ -16,7 +16,11 @@ const utils = @import("utils.zig");
 const vec_size = utils.get_SIMD_vector_size_in_bytes();
 
 
-fn search(haystack: []const u8, needle: []const u8) void {
+inline fn vector_bool_to_int(comptime T: type, comptime size: usize, vector: Vector(size, bool)) Vector(size, T) {
+    return @select(u8, vector, @splat(vec_size, @as(u8, 1)), @splat(vec_size, @as(u8, 0)));
+}
+
+pub fn search(haystack: []const u8, needle: []const u8) void {
     const f = @splat(vec_size, @as(u8, needle[0]));
     const l = @splat(vec_size, @as(u8, needle[needle.len-1]));
 
@@ -26,37 +30,26 @@ fn search(haystack: []const u8, needle: []const u8) void {
             const a: Vector(vec_size, u8) = haystack[i..][0..vec_size].*;
             const b: Vector(vec_size, u8) = haystack[i+needle.len-1..][0..vec_size].*;
 
-            const af = (a == f);
-            const bl = (b == l);
+            const af = vector_bool_to_int(u8, vec_size, (a == f));
+            const bl = vector_bool_to_int(u8, vec_size, (b == l));
 
-            // Hack: used to convert the bools to ints for a SIMD `and` operation. No other way
-            // to do this in Zig currently.
-            const x = @select(u8, af, @splat(vec_size, @as(u8, 1)), @splat(vec_size, @as(u8, 0)));
-            const y = @select(u8, bl, @splat(vec_size, @as(u8, 1)), @splat(vec_size, @as(u8, 0)));
-
-            var res = x&y;
-            if (@reduce(.Add, res) > 0) {
-
+            var afbl = af&bl;
+            if (@reduce(.Add, afbl) != 0) {
                 var j: usize = 0;
-                while (j < std.mem.len(res)) : (j += 1) {
-                    if (res[j] > 0) {
-                        print("{}", .{std.mem.eql(u8, haystack[i+j+1..i+j+needle.len-2], needle[1..needle.len-2])});
+                while (j < vec_size) : (j += 1) {
+                    if (afbl[j] != 0) {
+                        var r = std.mem.eql(u8, haystack[i+j+1..i+j+needle.len-1], needle[1..needle.len-1]);
+                        print("{}", .{r});
                     }
                 }
             }
-
-            print("{}:\n", .{i});
-            print("{}\n===\n", .{af});
-            print("{}\n===\n", .{bl});
-            print("{}\n===\n", .{x});
-            print("{}\n===\n", .{y});
-            print("{}\n===\n\n", .{x&y});
         }
     }
 }
 
+
 pub fn main() !void {
-    search("A pink and purple cat prances about the yard.", "cat");
+    search("A cAt clpt cat cet pink and purple cat cot cat! prances about the yard.", "cat");
 }
 
 
