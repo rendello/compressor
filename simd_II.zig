519ac20b840ecbb6cc55d68856dc24fc5a874467
Gaven Rendell <gaven@rendello.ca>
2021-10-02 13:44:30 -0400

    Further improvements to SIMD search

--- simd_II.zig
+++ simd_II.zig
@@ -14,19 +14,21 @@ const utils = @import("utils.zig");
 
 const vec_size = utils.get_SIMD_vector_size_in_bytes();
 
-
-inline fn vector_bool_to_int(comptime T: type, comptime size: usize, vector: Vector(size, bool)) Vector(size, T) {
-    return @select(u8, vector, @splat(vec_size, @as(u8, 1)), @splat(vec_size, @as(u8, 0)));
+inline fn vec_splat(byte: u8) Vector(vec_size, u8) {
+    return @splat(vec_size, @as(u8, byte));
 }
 
-inline fn vec_splat(byte: u8) Vector(16, u8) {
-    return @splat(16, @as(u8, byte));
-}
-
-inline fn vec_AND(vec_1: Vector(16, bool), vec_2: Vector(16, bool)) Vector(16, u8) {
-    const vec_1_int = @select(u8, vec_1, @splat(16, @as(u8, 1)), @splat(16, @as(u8, 0)));
-    const vec_2_int = @select(u8, vec_2, @splat(16, @as(u8, 1)), @splat(16, @as(u8, 0)));
+inline fn vec_AND(vec_1: Vector(vec_size, bool), vec_2: Vector(vec_size, bool)) Vector(vec_size, u8) {
+    const vec_1_int = @select(u8, vec_1, @splat(vec_size, @as(u8, 1)), @splat(vec_size, @as(u8, 0)));
+    const vec_2_int = @select(u8, vec_2, @splat(vec_size, @as(u8, 1)), @splat(vec_size, @as(u8, 0)));
     return vec_1_int & vec_2_int;
+}
+
+inline fn quick_eql(a: []const u8, b: []const u8) bool {
+    for (a) |item, index| {
+        if (b[index] != item) return false;
+    }
+    return true;
 }
 
 const Match = struct {
@@ -45,7 +47,7 @@ inline fn check_potential_match(
     var target_len = outer_target_len;
 
     // (No need to compare first and last bytes, they've been checked.)
-    if (std.mem.eql(u8, window[match_start+1..match_start+target_len-1], search_buff[1..target_len-1])) {
+    if (quick_eql(window[match_start+1..match_start+target_len-1], search_buff[1..target_len-1])) {
         target_len += 1;
         while (target_len < search_buff.len and window[match_start+target_len] == search_buff[target_len]) {
             target_len += 1;
@@ -64,10 +66,10 @@ fn find_best_match(window: []const u8, search_buff: []const u8) ?Match {
     const first_byte_mask = vec_splat(search_buff[0]);
     var last_byte_mask = vec_splat(search_buff[target_len]);
 
-    var index: usize = (window.len - 16) - target_len;
+    var index: usize = (window.len - vec_size) - target_len;
     while (true) {
-        const first_block: Vector(16, u8) = window[index..][0..16].*;
-        const last_block:  Vector(16, u8) = window[index+target_len..][0..16].*;
+        const first_block: Vector(vec_size, u8) = window[index..][0..vec_size].*;
+        const last_block:  Vector(vec_size, u8) = window[index+target_len..][0..vec_size].*;
 
         const eq_first = (first_byte_mask == first_block);
         const eq_last = (last_byte_mask == last_block);
@@ -75,8 +77,8 @@ fn find_best_match(window: []const u8, search_buff: []const u8) ?Match {
         const mask = vec_AND(eq_first, eq_last);
 
         if (@reduce(.Add, mask) != 0) {
-            var mask_index: usize = 16-1;
-            while (mask_index > 0) : (mask_index -= 1) {
+            var mask_index: usize = vec_size-1;
+            while (true) {
                 if (mask[mask_index] != 0) {
                     var match_opt = check_potential_match(window, search_buff, index, mask_index, target_len);
                     if (match_opt) |match| {
@@ -91,10 +93,15 @@ fn find_best_match(window: []const u8, search_buff: []const u8) ?Match {
                         }
                     }
                 }
+                if (mask_index > 0) {
+                    mask_index -= 1;
+                } else {
+                    break;
+                }
             }
         }
-        if (index > 16) {
-            index -= 16;
+        if (index > vec_size) {
+            index -= vec_size;
         } else if (index == 0) {
             break;
         } else {
@@ -108,7 +115,7 @@ fn find_best_match(window: []const u8, search_buff: []const u8) ?Match {
 test "find best match" {
     {
         var best_match = find_best_match("It was the best of times, it was the blurst of times.", "blurst");
-        try expect(best_match.?.len == 6 and best_match.?.distance == 16);
+        try expect(best_match.?.len == 6 and best_match.?.distance == vec_size);
     }
     {
         var best_match = find_best_match("It is tea time, teatime, time for teas.", "teather");
@@ -116,7 +123,7 @@ test "find best match" {
     }
 }
 
-test "find best match prefers last occurence" {
+test "find best match prefers rightmost occurence" {
     {
         var best_match = find_best_match("Tea tea tea teatime tea teatime tea", "teatime");
         try expect(best_match.?.len == 7 and best_match.?.distance == 11);
@@ -127,11 +134,14 @@ test "find best match prefers last occurence" {
     }
 }
 
+test "find best match at beginning" {
+    var best_match = find_best_match("Hello, is there anybody in there?", "Hello?");
+    try expect(best_match.?.len == 5 and best_match.?.distance == 33);
+}
+
 pub fn main() !void {
-    //var best_match = find_best_match("It was the best of times, it was the blurst of times.", "blurst");
+    //var best_match = find_best_match(@embedFile("bible.txt"), "Jesus");
     //print("{}", .{best_match});
-    var best_match = find_best_match(@embedFile("bible.txt"), "Jesus");
-    print("{}", .{best_match});
 }
 
 
